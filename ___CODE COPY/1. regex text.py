0c414f2411bd344f138cdcc0f2f528a8a4f6448d
Nigel Bell <nigelthomasbell@gmail.com>
2022-05-30 17:29:50 +1200

    (dev) v8.3

--- ___CODE COPY/1. regex text.py
+++
@@ -1,7 +0,0 @@
-import re
-files = ["ax//x", "bd;\\a", "a\/cb:a"]
-badChars = [":", ";"]
-for file in files:
-    subfolders = re.split("[/\\\\]+", file)
-    if any(any(char in badChars for char in item) for item in subfolders):
-        print(subfolders)
